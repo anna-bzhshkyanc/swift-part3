317e37356021eb2e72029f95759a4d5edfd67856
Anna Bzhshkyanc <annabzhshkyan@192.168.27.3>
2025-06-03 13:24:34 +0400

    task 2

--- swift-part3/main.swift
+++ swift-part3/main.swift
@@ -99,3 +99,45 @@ class Library {
 
 
 
+//task 2
+struct School {
+    enum SchoolRole {
+        case student
+        case teacher
+        case administrator
+    }
+    
+    class Person {
+        let name: String
+        var role: SchoolRole
+        
+        init(name: String, role: SchoolRole) {
+            self.name = name
+            self.role = role
+        }
+    }
+    
+    var people: [Person] = []
+    
+    subscript(role: SchoolRole) -> [Person] {
+        return people.filter { $0.role == role}
+    }
+    
+    mutating func addPerson(_ person: Person) {
+        people.append(person)
+    }
+}
+
+func countStudents(school: School) -> Int {
+    return school[.student].count
+}
+
+
+func countTeachers(school: School) -> Int {
+    return school[.teacher].count
+}
+
+func countAdministrators(school: School) -> Int {
+    return school[.administrator].count
+}
+
