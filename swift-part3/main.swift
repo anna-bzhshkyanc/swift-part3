c0abc9d4a7402864545e04cd4f0efac036dad939
Anna Bzhshkyanc <annabzhshkyan@192.168.27.3>
2025-06-03 13:27:54 +0400

    task 3

--- swift-part3/main.swift
+++ swift-part3/main.swift
@@ -141,3 +141,39 @@ func countAdministrators(school: School) -> Int {
     return school[.administrator].count
 }
 
+
+//task 3
+let books = [
+["title": "Swift Fundamentals", "author": "John Doe", "year": 2015, "price": 40, "genre": ["Programming", "Education"]],
+["title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925, "price": 15, "genre": ["Classic", "Drama"]],
+["title": "Game of Thrones", "author": "George R. R. Martin", "year": 1996, "price": 30, "genre": ["Fantasy", "Epic"]],
+["title": "Big Data, Big Dupe", "author": "Stephen Few", "year": 2018, "price": 25, "genre": ["Technology", "Non-Fiction"]],
+["title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960, "price": 20, "genre": ["Classic", "Drama"]]
+]
+
+let discountedPrices: Array<Double> = books.compactMap {
+    if let price = $0["price"] as? Int {
+        return Double(price) * 0.9
+    } else { return nil }
+}
+
+let booksPostedAfter2000: Array<String>  = books.filter {
+    if let year = $0["year"] as? Int {
+        return year > 2000
+    } else { return false }
+}.compactMap {
+    $0["title"] as? String
+}
+
+let allGenres: Set<String> = Set(
+    books.compactMap({$0["genre"] as? [String]}).flatMap{$0}
+)
+
+
+let priceInt = books.compactMap{$0["price"] as? Int}
+let totalCost: Int = priceInt.reduce(0) {$0 + $1}
+
+
+
+
+
